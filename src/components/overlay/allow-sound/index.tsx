ba2f076b7597fadd3078a5b9bcf4ccc2f2e1a95e
O-RGB <ok.oza5555@gmail.com>
2024-01-25 18:13:12 +0700

    add ui permission

--- src/components/overlay/allow-sound/index.tsx
+++ src/components/overlay/allow-sound/index.tsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from "react";
+import Wallpaper from "../../wallpaper";
 
 interface AllowSoundProps {
   children: React.ReactNode;
 }
 
 const AllowSound: React.FC<AllowSoundProps> = ({ children }) => {
-  const [ended, setEnded] = useState(false);
+  const [ended, setEnded] = useState<boolean>(false);
   const [pressed, setPressed] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
 
@@ -26,23 +27,35 @@ const AllowSound: React.FC<AllowSoundProps> = ({ children }) => {
 
   return (
     <>
-      <div>
-        {!pressed && ended == false && (
-          <div
-            onClick={handleClick}
-            className="p-3 rounded-lg border cursor-pointer bg-white"
-          >
-            เปิดเสียงเบราว์เซอร์
-          </div>
-        )}
+      <Wallpaper blur>
+        <div>
+          {
+            <div className="relative ">
+              <div
+                onClick={handleClick}
+                className="p-3 rounded-lg border cursor-pointer bg-white hover:bg-gray-200 duration-300 flex justify-center items-center gap-3"
+              >
+                <span className="relative flex h-3 w-3">
+                  <span className="animate-ping absolute h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                  <span className="relative rounded-full h-3 w-3 bg-sky-500"></span>
+                </span>
+                {pressed == false ? (
+                  <div>เปิดเสียงเบราว์เซอร์</div>
+                ) : (
+                  <div>กำลังโหลด</div>
+                )}
+              </div>
+            </div>
+          }
 
-        <audio
-          src="/ping.mp3"
-          controls={false}
-          autoPlay={false}
-          ref={audioRef}
-        />
-      </div>
+          <audio
+            src="/ping.mp3"
+            controls={false}
+            autoPlay={false}
+            ref={audioRef}
+          />
+        </div>
+      </Wallpaper>
     </>
   );
 };
--- src/components/wallpaper/index.tsx
+++ src/components/wallpaper/index.tsx
@@ -3,14 +3,21 @@ import React, { useEffect } from "react";
 
 interface WallpaperProps {
   children?: React.ReactNode;
+  blur?: boolean;
 }
 
-const Wallpaper: React.FC<WallpaperProps> = ({ children }) => {
+const Wallpaper: React.FC<WallpaperProps> = ({ children, blur }) => {
   return (
     <>
-      <div className="relative h-screen w-screen overflow-hidden flex justify-center items-center">
+      <div
+        className={`relative h-screen w-screen overflow-hidden flex justify-center items-center `}
+      >
         <div className="absolute z-50 ">{children}</div>
-        <div className="absolute h-full w-full">
+        <div
+          className={`absolute h-full w-full ${
+            blur ? "blur-lg scale-105 opacity-80" : ""
+          }`}
+        >
           <div className="bg-black absolute opacity-10 h-full w-full"></div>
           <img
             src="/wallpaper.jpeg"
