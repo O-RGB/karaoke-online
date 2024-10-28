da227b65f85401a0ee166864f612590009f1fb23
phichayoot <phichayoot@digitiveworks.com>
2024-10-28 21:41:21 +0700

    ลองย้ายไฟล์

--- next.config.mjs
+++ next.config.mjs
@@ -7,4 +7,8 @@ import withPWA from "next-pwa";
 
 export default withPWA({
   dest: "public",
+  sw: "synthetizer/worklet_processor.min.js",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
