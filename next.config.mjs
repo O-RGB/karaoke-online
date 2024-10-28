040f60203a0387be66273899e7e2487928e56d8d
phichayoot <phichayoot@digitiveworks.com>
2024-10-28 22:06:49 +0700

    ทดสอบ conig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,37 @@
+// import withPWA from "next-pwa";
+
+// export default withPWA({
+//   //   sw: "synthetizer/worklet_processor.min.js",
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development",
+//   register: true,
+//   skipWaiting: true,
+// });
+
 // /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
 
-// export default nextConfig;
-
+// module.exports = withPWA(nextConfig);
 import withPWA from "next-pwa";
 
-export default withPWA({
+const pwaConfig = withPWA({
   dest: "public",
   sw: "synthetizer/worklet_processor.min.js",
+  disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default {
+  ...pwaConfig,
+  ...nextConfig,
+};
